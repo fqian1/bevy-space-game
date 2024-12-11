f572dd138e10d7aaf8cbba9afc7bb30a3e13bcb6
Francois Qian <fqian@Francoiss-MacBook-Pro.local>
2024-12-11 11:38:05 +0000

    Acceleration

--- src/main.rs
+++ src/main.rs
@@ -12,22 +12,35 @@ struct Velocity {
     y: f32,
 }
 
+#[derive(Component, Debug)]
+struct Acceleration {
+    x: f32,
+    y: f32,
+}
+
 fn main() {
     App::new()
     .add_systems(Startup, spawn_spaceship)
-    .add_systems(Update, (update_position, print_position))
+    .add_systems(Update, ((update_velocity, update_position).chain(), print_position))
     .add_plugins(DefaultPlugins)
     .run();
 }
 
 fn spawn_spaceship(mut commands: Commands) {
-    commands.spawn((Position{x: 0.0, y: 0.0}, Velocity{x: 1.0, y: 1.0}));
+    commands.spawn((Position{x: 0.0, y: 0.0}, Velocity{x: 1.0, y: 1.0}, Acceleration{x: 0.1, y: 0.1}));
 }
 
 fn update_position(mut query: Query<(&Velocity, &mut Position)>) {
     for (velocity, mut position) in query.iter_mut() {
         position.x += velocity.x;
         position.y += velocity.y;
+    }
+}
+
+fn update_velocity(mut query: Query<(&Acceleration, &mut Velocity)>) {
+    for (acceleration, mut velocity) in query.iter_mut() {
+        velocity.x += acceleration.x;
+        velocity.y += acceleration.y;
     }
 }
 
