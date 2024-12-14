5105cb101340129f79d9c55ec5658a941a53926f
Francois Qian <fqian@Francoiss-MacBook-Pro.local>
2024-12-14 08:52:29 +0000

    spaceship MOVING BIG missiles

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,27 @@ mod debug;
 mod movement;
 mod spaceship;
 mod camera;
+mod asteroid;
+mod asset_loader;
 
 use bevy::prelude::*;
 use spaceship::SpaceshipPlugin;
 use debug::DebugPlugin;
 use movement::MovementPlugin;
 use camera::CameraPlugin;
+use asteroid::AsteroidPlugin;
+use asset_loader::AssetLoaderPlugin;
 
 
 fn main() {
     App::new()
-    .insert_resource(ClearColor(Color::srgb(0., 0., 0.)))
-    .insert_resource(AmbientLight{
-        brightness: 0.75,
-        ..Default::default()
-    })
+    .insert_resource(ClearColor(Color::srgb(0.1, 0.1, 0.1)))
     .add_plugins(DefaultPlugins)
     .add_plugins(SpaceshipPlugin)
     .add_plugins(MovementPlugin)
     .add_plugins(DebugPlugin)
+    .add_plugins(AssetLoaderPlugin)
     .add_plugins(CameraPlugin)
+    .add_plugins(AsteroidPlugin)
     .run();
 }
